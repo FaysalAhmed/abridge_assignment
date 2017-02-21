a499911aeaf10618f249c8b019a053db0a98b9bd
Faysal Ahmed <faysal.ahmed833@gmail.com>
2017-02-21 23:12:33 +0600

    added updated sql

--- db.sql
+++ db.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.1.4
+-- version 4.5.5.1
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 19, 2017 at 07:13 AM
--- Server version: 5.6.15-log
--- PHP Version: 5.5.8
+-- Generation Time: Feb 21, 2017 at 06:12 PM
+-- Server version: 5.7.11
+-- PHP Version: 7.0.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -13,6 +13,33 @@ SET time_zone = "+00:00";
 --
 -- Database: `abridge`
 --
+CREATE DATABASE IF NOT EXISTS `abridge` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `abridge`;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `comments`
+--
+
+DROP TABLE IF EXISTS `comments`;
+CREATE TABLE `comments` (
+  `id` int(11) NOT NULL,
+  `thread_id` int(11) NOT NULL,
+  `commented_by` int(11) NOT NULL,
+  `text` text NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `comments`
+--
+
+INSERT INTO `comments` (`id`, `thread_id`, `commented_by`, `text`) VALUES
+(1, 1, 1, 'hello'),
+(2, 1, 1, 'hello'),
+(3, 1, 1, 'asd'),
+(9, 3, 1, 'hello comment'),
+(11, 3, 2, 'hello 3');
 
 -- --------------------------------------------------------
 
@@ -20,15 +47,23 @@ SET time_zone = "+00:00";
 -- Table structure for table `threads`
 --
 
-CREATE TABLE IF NOT EXISTS `threads` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `threads`;
+CREATE TABLE `threads` (
+  `id` int(11) NOT NULL,
   `name` text NOT NULL,
   `creator` text NOT NULL,
   `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `text` text NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+  `text` text NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `threads`
+--
+
+INSERT INTO `threads` (`id`, `name`, `creator`, `created_at`, `updated_at`, `text`) VALUES
+(3, 'asd21', '1', '2017-02-21 16:07:44', '2017-02-21 16:07:25', '<p>asd</p>\r\n'),
+(4, 'from faysal2 1', '2', '2017-02-21 16:58:17', '2017-02-21 16:57:39', '<p>this is a demo &nbsp;asd</p>\r\n');
 
 -- --------------------------------------------------------
 
@@ -36,16 +71,60 @@ CREATE TABLE IF NOT EXISTS `threads` (
 -- Table structure for table `users`
 --
 
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(20) NOT NULL,
   `username` text NOT NULL,
-  `password` text NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+  `password` text NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `username`, `password`) VALUES
-(1, 'faysal', 'e10adc3949ba59abbe56e057f20f883e');
+(1, 'faysal', 'e10adc3949ba59abbe56e057f20f883e'),
+(2, 'faysal2', 'e10adc3949ba59abbe56e057f20f883e'),
+(3, 'fa', 'd41d8cd98f00b204e9800998ecf8427e');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `threads`
+--
+ALTER TABLE `threads`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+--
+-- AUTO_INCREMENT for table `threads`
+--
+ALTER TABLE `threads`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
